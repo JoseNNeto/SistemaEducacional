b1a0fdecdd3762443d8d0125f045889d6c6bc7d2
José Neto <jnan@discente.ifpe.edu.br>
2025-05-13 23:18:36 -0300

    Inicio Projeto BackEnd

---
+++ backend/sitemaeducacional/src/main/java/com/educacional/sitemaeducacional/SitemaeducacionalApplication.java
@@ -0,0 +1,13 @@
+package com.educacional.sitemaeducacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemaeducacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemaeducacionalApplication.class, args);
+	}
+
+}
---
+++ backend/sitemaeducacional/src/test/java/com/educacional/sitemaeducacional/SitemaeducacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educacional.sitemaeducacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitemaeducacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
