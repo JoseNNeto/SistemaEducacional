a13641dcd668e784326c41e806c48dfd9b213df2
José Neto <jnan@discente.ifpe.edu.br>
2025-05-28 23:05:29 -0300

    feat: add Security Configuration

--- backend/sitemaeducacional/src/main/java/com/educacional/sitemaeducacional/repository/UsuarioRepository.java
+++ backend/sitemaeducacional/src/main/java/com/educacional/sitemaeducacional/repository/UsuarioRepository.java
@@ -1,5 +1,8 @@
 package com.educacional.sitemaeducacional.repository;
 
+import java.lang.foreign.Linker.Option;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,7 +10,7 @@ import com.educacional.sitemaeducacional.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    
+    Optional<Usuario> findByEmail(String email);
     // Aqui você pode adicionar métodos personalizados de consulta, se necessário.
     // Por exemplo, para encontrar um usuário por email:
     // Optional<Usuario> findByEmail(String email);
